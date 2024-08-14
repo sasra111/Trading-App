620d5f356b2b094701875a5e2fd9f1e60f75f08f
Sasith Bandara <sasithbandara12@gmail.com>
2024-08-14 21:12:57 +0530

    First Commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+using namespace std;
+
+int main() {
+    cout << "1. Help" << endl;
+    cout << "2. Status" << endl;  
+    cout << "3. Make Offer" << endl;  
+    cout << "4. Make Bid" << endl;  
+    cout << "5. Wallet" << endl;  
+    cout << "6. Continue" << endl;
+    return 0; 
+}
