4198a6d1d6e86d44f944dcff9b70bd4a2dbee331
sasra111 <sasithbandara12@gmail.com>
2024-08-18 13:05:57 +0530

    3rd Commit

--- main.cpp
+++ main.cpp
@@ -2,12 +2,49 @@
 using namespace std;
 
 int main() {
-    cout << "1. Help" << endl;
-    cout << "2. Status" << endl;  
-    cout << "3. Make Offer" << endl;  
-    cout << "4. Make Bid" << endl;  
-    cout << "5. Wallet" << endl;  
-    cout << "6. Continue" << endl;
-    cout << "6. Test" << endl;
-    return 0; 
+    int userOption;
+    do{
+        cout << "1. Help" << endl;
+        cout << "2. Exchange Status" << endl;  
+        cout << "3. Make Offer" << endl;  
+        cout << "4. Make Bid" << endl;  
+        cout << "5. Wallet" << endl;  
+        cout << "6. Continue" << endl; 
+
+        cout<<"===============" << endl;
+        cout<<"Type in 1-6 : " ;
+        cin >> userOption ;
+
+        switch (userOption)
+        {
+        case 0:
+            cout << "Exit." <<endl;
+            break;
+        case 1:
+            cout << "Help - choose options from the menu and follow the on screen instructions." <<endl;
+            break;
+        case 2:
+            cout << "Exchange Status" <<endl;
+            break;
+        case 3:
+            cout << "Make an offer." <<endl;
+            break;
+        case 4:
+            cout << "Make b bid." <<endl;
+            break;
+        case 5:
+            cout << "Wallet Information" <<endl;
+            break;
+        case 6:
+            cout << "Continue Trading" <<endl;
+            break;
+        default:
+            cout << "Wrong Input. Enter 1-6" <<endl;
+            break;
+        }
+        cout << "" << endl;
+
+    }
+    while (userOption);
+    return 0;
 }
